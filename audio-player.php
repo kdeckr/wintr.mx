beaf0a1fb2cb4fc866a9f65728438cae2f6f6b11
patternicity <Kyle@kyles-mbp-4.att.net>
2013-12-07 17:43:35 -0500

    Remove number from filename, edit progress bar

--- audio-player.php
+++ audio-player.php
@@ -9,7 +9,7 @@
 				foreach (glob($dir."*.mp3") as $filename) {
 					$i++;
 					$filename_cut = substr ($filename, strlen($dir));
-					$noext = substr ($filename_cut, 0, -4);
+					$noext = substr ($filename_cut, 3, -4);
 					echo('
 					<audio id="song'.$i.'" src="'.$dir.$filename_cut.'" name="'.$noext.'" type="audio/mpeg"></audio>
 					');
@@ -19,7 +19,7 @@
 		<div id="display">
 			<div id="title" class="display_text">No Song Data Found</div>
 		</div> <!-- /#display -->
-		<div id	="progressbar_container"><div id="progressbar_buffer"></div><div id="progressbar"></div></div>
+		<div id	="progressbar_container"><div id="progressbar"></div></div>
 		<div id="timer" class="display_text">0:00</div>
 		<div id="controls">
 			<div class="ctrl_btn" id="prev">Prev</div>
@@ -31,20 +31,19 @@
 	</div> <!-- /#player -->
 
 	<!-- Download -->
-	<a href=<?php echo ("/".$year."/songs/WinterMix".$year.".zip"); ?>>Download Album</a>
+	<a href=<?php echo ("/".$year."/songs/WinterMix".$year.".zip"); ?> id="download">Download Album</a>
 </section>
 
 <section id="right">
-	<?php
-		/* DISPLAY PLAYLIST */
-		echo "<ol>";
-		foreach (glob($dir."*.mp3") as $filename) {
-			$filename_cut = substr ($filename, strlen($dir));
-			$noext = substr ($filename_cut, 0, -4);
-			$title = substr ($noext, 3);
-			
-			echo "<li>".$title."</li>";
-		}
-		echo "</ol>";
-	?>
+	<ol id="tracklist">
+		<?php
+			foreach (glob($dir."*.mp3") as $filename) {
+				$filename_cut = substr ($filename, strlen($dir));
+				$noext = substr ($filename_cut, 0, -4);
+				$title = substr ($noext, 3);
+				
+				echo "<li>".$title."</li>";
+			}
+		?>
+	</ol>
 </section>
