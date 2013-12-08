228d0029982ca0dceaa54e5f6c65f85834e095d5
patternicity <Kyle@kyles-mbp-4.att.net>
2013-12-07 21:34:23 -0500

    Parallax scrolling, yo

--- footer.php
+++ footer.php
@@ -3,16 +3,17 @@
 	</div> <!-- /#wrapper -->
 	
 	<script language="javascript" type="text/javascript" src="../audio-player.js"></script>
-	<!-- Parallax Scroll Background
+
+	<!-- Parallax Scroll Background -->
 	<script language="javascript" type="text/javascript">
 		$(window).scroll(function(e){
 			parallax();
 		});
 		function parallax(){
 			var scrolled = $(window).scrollTop();
-			$('body').css('background-position','center -'+(scrolled*0.2)+'px');
+			$('body').css('background-position','center -'+(scrolled*0.2)+'px'); /* Change decimal to alter parallax speed. */
 		}
-	</script>-->
+	</script>
  	<section id="footer">
 		<p>A <a href="http://kyledecker.me">Kyle Decker</a> project, crafted with love.</p>
 		<p>Please support the artists that you enjoy!</p>
